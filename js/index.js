fe0b33a7ddb1e5338ce848d98865b191d5e8836a
jaychou <747305307@qq.com>
2020-04-13 01:50:19 +0800

    修改内容

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ class Coder{
 	//显示文字
 	load(code,second){
 		let _dom = this.dom
-		let _second = second ? second : 700
+		let _second = second ? second : 400
 		_second = m + _second
 		setTimeout(function(){
 			$('#'+_dom).scrollTop($('#'+_dom).get(0).scrollHeight)
@@ -49,17 +49,29 @@ class Coder{
 
 
 $(function(){
+	var width = document.documentElement.clientWidth;
+	var html = document.querySelector('html')
+	
+	$(window).resize(function(){
+		
+		html.style.fontSize = 10 * (width / 1920) + 'px'
+		console.log(html.style.fontSize);
+	})
+	
+	
 	let c = new Coder('work')
 	c.load('<p>&nbsp;</p>')
-	c.load('<p class="writecode work">你好</p>')
-	c.load('<p  class="writecode  work">我叫周善斌,毕业于大连民族大学计算机科学与工程学院</p>')
-	c.load('<p  class="writecode  work">专业是计算机科学与技术</p>')
-	c.load('<p  class="writecode work">目前求职意向为前端工程师</p>')
-	c.load('<p  class="writecode work">现在我将用完成我的简历</p>')
-	c.load('<p  class="writecode work">请您过目</p>')
+	c.load('<p class="writecode work">你好</p>',1200)
+	c.load('<p  class="writecode  work">我叫周善斌,毕业于大连民族大学计算机科学与工程学院</p>',1200)
+	c.load('<p  class="writecode  work">专业是计算机科学与技术</p>',1200)
+	c.load('<p  class="writecode work">目前求职意向为前端工程师</p>',1200)
+	c.load('<p  class="writecode work">如果您愿意用两分钟时间看完我的这份简历将是我莫大的荣幸，感谢！</p>',1200)
+	c.load('<p  class="writecode work">这份简历将尽力还原我所投递的简历附件</p>',1200)
+	c.load('<p  class="writecode work">现在我将用完成我的简历</p>',1200)
+	c.load('<p  class="writecode work">请您过目</p>',1200)
 	
 	c.load('<p>&nbsp;</p>')
-	c.load('<p  class="writecode work">首先让我们做点改变</p>')
+	c.load('<p  class="writecode work">首先让我们做点改变</p>',800)
 	c.load('<p>&nbsp;</p>')
 	
 	c.load('<p><choice>html</choice><redc>{</redc></p>')
@@ -71,8 +83,8 @@ $(function(){
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
 	
-	c.load('<p class="writecode work">&nbsp;&nbsp;现在黑字体显示不清楚了</p>')
-	c.load('<p class="writecode work">&nbsp;&nbsp;让我们换一种字体</p>')	
+	c.load('<p class="writecode work">&nbsp;&nbsp;现在黑字体显示不清楚了</p>',800)
+	c.load('<p class="writecode work">&nbsp;&nbsp;让我们换一种字体</p>',800)	
 	
 	c.load('<p>&nbsp;</p>')
 	c.load('<p><choice>#main,a</choice><redc>{</redc></p>')
@@ -84,8 +96,8 @@ $(function(){
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
 	
-	c.load('<p class="writecode work">好多了,但是这样看起来还是不对劲</p>')
-	c.load('<p class="writecode work">接下来我创建一个区域来更好的显示我的代码</p>')
+	c.load('<p class="writecode work">好多了,但是这样看起来还是不对劲</p>',800)
+	c.load('<p class="writecode work">接下来我创建一个区域来更好的显示我的代码</p>',800)
 	
 	c.load('<p>&nbsp;</p>')
 	c.load('<p><choice>#work</choice><redc>{</redc></p>')
@@ -148,8 +160,8 @@ $(function(){
 	c.load('<p>&nbsp;</p>')
 	
 	
-	c.load('<p class="writecode work">现在我们发现代码的显示效果太差了</p>')
-	c.load('<p class="writecode work">让我们来换一种</p>')
+	c.load('<p class="writecode work">现在我们发现代码的显示效果太差了</p>',800)
+	c.load('<p class="writecode work">让我们来换一种</p>',800)
 	
 	c.load('<p>&nbsp;</p>',2000)
 	c.addClass("choice","choice")
@@ -158,7 +170,7 @@ $(function(){
 	c.addClass("val","val")
 	
 	
-	c.load('<p  class="writecode work">现在我们给它加点立体效果</p>')
+	c.load('<p  class="writecode work">现在我们给它加点立体效果</p>',800)
 	
 	c.load('<p>&nbsp;</p>')
 	c.load('<p><choice>#main</choice><redc>{</redc></p>')
@@ -168,9 +180,10 @@ $(function(){
 		"val": "1000px"
 	}])
 	c.addClass("choice","choice")
-	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
+
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
 	
@@ -185,6 +198,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>max-height</name> <redc>:</redc> <val>85%;</val></p>')
 	c.setClass(".code-work", [{
@@ -195,12 +209,12 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
-	
+	c.addClass("redc","redc")
 	
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
 	
-	c.load('<p  class="writecode work">接下来我将正式的开始制作我的简历</p>')
+	c.load('<p  class="writecode work">接下来我将正式的开始制作我的简历</p>',800)
 	
 	c.load('<p>&nbsp;</p>')
 	c.load('<p><choice>.code-resume</choice><redc>{</redc></p>')
@@ -214,6 +228,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>right</name> <redc>:</redc> <val>0;</val></p>')
 	c.setClass(".code-resume", [{
@@ -224,6 +239,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>-webkit-perspective</name> <redc>:</redc> <val>1000px;</val></p>')
 	c.setClass(".code-resume", [{
@@ -234,6 +250,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>width</name> <redc>:</redc> <val>45%;</val></p>')
 	c.setClass(".code-resume", [{
@@ -244,6 +261,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>height</name> <redc>:</redc> <val>85%;</val></p>')
 	c.setClass(".code-resume", [{
@@ -254,6 +272,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>max-height</name> <redc>:</redc> <val>85%;</val></p>')
 	c.setClass(".code-resume", [{
@@ -264,6 +283,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>background-color</name> <redc>:</redc> <val>#FFFFE0;</val></p>')
 	c.setClass(".code-resume", [{
@@ -274,6 +294,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>-webkit-transform</name> <redc>:</redc> <val>translateX(-8%) rotateY(-10deg) translateY(5%);</val></p>')
 	c.setClass(".code-resume", [{
@@ -284,6 +305,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>border</name> <redc>:</redc> <val>1px soild #ccc;</val></p>')
 	c.setClass(".code-resume", [{
@@ -294,6 +316,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>padding</name> <redc>:</redc> <val>10px 10px 20px;</val></p>')
 	c.setClass(".code-resume", [{
@@ -304,6 +327,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p>&nbsp; <name>box-shadow</name> <redc>:</redc> <val>-4px 4px 2px 0 rgba(0,0,0,0.3);</val></p>')
 	c.setClass(".code-resume", [{
@@ -314,13 +338,13 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
-	
+	c.addClass("redc","redc")
 	
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
 	
 	
-	c.load('<p  class="writecode work">布局完成,接下来开始加入内容</p>')
+	c.load('<p  class="writecode work">布局完成,接下来开始加入内容</p>',800)
 	
 
 	let m = new Coder('coder')
@@ -338,6 +362,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
@@ -351,6 +376,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
@@ -376,6 +402,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
@@ -391,6 +418,7 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
@@ -400,54 +428,50 @@ $(function(){
 	c.load('<p><choice>.weight</choice><redc>{</redc></p>')
 	c.load('<p>&nbsp; <name>font-weight</name> <redc>:</redc> <val>500;</val></p>')
 	
+	c.load('<p><redc>}</redc></p>')
+	c.load('<p>&nbsp;</p>')
+	
 	c.addClass("choice","choice")
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
-	
-	c.load('<p><redc>}</redc></p>')
-	c.load('<p>&nbsp;</p>')
+	c.addClass("redc","redc")
 	
 	
 	l.load('<p>&nbsp;</p>')
-	l.load('<h1 class="writecode">教育背景</h1>')
+	l.load('<h1 class="writecode">专业技能</h1>')
 	l.load('<p>&nbsp;</p>')
-	l.load('<p class="writecode">2015.09-2019.06   大连民族大学     计算机科学与技术      本科</p>')
+	l.load('<p class="writecode">熟悉Vue框架以及Vue-Router，Vuex等vue全家桶的使用，熟悉组件化开发。</p>')
+	l.load('<p class="writecode">熟练使用uni-app开发跨平台应用，理解微信小程序的开发，有开发小程序和跨平台应用的经验。</p>')
+	l.load('<p class="writecode">熟练使用Html,CSS,JavaScript等开发语言，熟悉jQuery等JS库，熟悉CSS3新特性，ES6新特性，进行可读性高的W3C规范代码编程。</p>')
+	l.load('<p class="writecode">理解Vant，Element，Bootstrap等组件库的使用，能快速上手搭建页面。</p>')
+	l.load('<p class="writecode">熟悉git版本管理工具的使用，熟悉正则表达式，理解webpack打包工具的使用，了解node后台开发。</p>')
 	l.load('<p>&nbsp;</p>')
 	
 	
 	l.load('<h1 class="writecode">工作经历</h1>')
 	l.load('<p>&nbsp;</p>')
-	l.load('<p class="writecode weight">2019.02-2019.06   亚信科技福州分公司 亚信安全部门  web开发实习生</p>')
-	l.load('<p class="writecode">职责:与技术团队等进行充分的沟通，配合后台开发人员实现产品前端界面效果与功能。负责部分产品HTML5、CSS3、JS的编写，解决响应适配等问题，确保产品具有优质的用户使用体验。</p>')
-	
-	l.load('<p>&nbsp;</p>')
-	l.load('<h1 class="writecode">项目经历</h1>')
-	l.load('<p>&nbsp;</p>')
-	l.load('<p class="writecode weight">《WGCLOUDE》</p>')
-	l.load('<p class="writecode">项目描述：一款linux性能监测工具，用于检查网络吞吐率，服务器cpu监控，内存监控等。</p>')
-	l.load('<p class="writecode">责任描述：此项目为团队项目，本人主要负责部分页面的布局，javascript逻辑的实现和后期一些升级优化。</p>')
-	l.load('<p class="writecode">技术要点：1.使用Vue框架，Element-ui搭建整体架构。2.使用ECharts可视化工具进行数据可视化展示 3.通过AJAX获取后台数据，并进行数据解析，再到页面进行展示。</p>')
+	l.load('<p class="writecode weight">南京臻超科技有限公司&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2019.07-2020.01</p>')
+	l.load('<p class="writecode">项目名称: 科创中心</p>')
+	l.load('<p class="writecode">项目介绍: 一款辅助建筑施工人员现场施工的App软件</p>')
+	l.load('<p class="writecode">主要工作：负责考勤打卡和环境检测模块的开发。主要使用flex布局，通过request和后台交互，使用getLocation获取地理位置，使用map标签绘制地图展示用户考勤打卡信息，使用uni-app的ucharts绘制图表进行可视化的展示，图标主要展示用户的打卡记录，缺卡请假等信息。</p>')
+	l.load('<p class="writecode">技术栈：uni-app,ucharts</p>')
 	l.load('<p>&nbsp;</p>')
 	
-	l.load('<p class="writecode weight">《电商客户端管理系统》</p>')
-	l.load('<p class="writecode">项目描述：一款移动端的电商客户端，主要使用Vue全家桶开发。</p>')
-	l.load('<p class="writecode">责任描述：此项目为个人毕设项目，由本人独立完成。</p>')
-	l.load('<p class="writecode">技术要点：1.使用Vue，Mint-ui搭建整体架构。2.使用Vue-router搭建路由，Vuex存储状态，Axios请求数据 3.使用better-scroll实现上拉刷新下拉加载，使用swiper实现轮播图。4.使用基于Node的Express框架搭建后台。</p>')
+	l.load('<p class="writecode">项目名称：智慧工地</p>')
+	l.load('<p class="writecode">项目介绍：一款控制中心大屏展示施工进度的施工系统</p>')
+	l.load('<p class="writecode">主要工作：负责页面整体布局，按照要求还原设计图。该系统主要使用Html，CSS，JavaScript还原项目，使用了jQuery库辅助开发，使用响应式布局和flex布局优化页面，实现了在施工现场控制中心展示环境监测，塔吊，高支模，地磅，升降机等数据的展示。</p>')
+	l.load('<p class="writecode">技术栈：Html，CSS，JavaScript，jQuery</p>')
 	l.load('<p>&nbsp;</p>')
 	
-	l.load('<h1 class="writecode">校园实践</h1>')
-	l.load('<p>&nbsp;</p>')
-	l.load('<p class="writecode weight">2016.09-2018.09		         创软工作室	  </p>')
-	l.load('<p class="writecode">加入大连民族大学创软工作室,小组核心技术人员,在工作室中参加学校举办的软件设计活动,负责网站制作部分,培养了团队协作能力。</p>')
 	
+	l.load('<p class="writecode weight">亚信科技有限公司&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2018.07-2019. 05</p>')
+	l.load('<p class="writecode">项目名称：WGCLOUDE</p>')
+	l.load('<p class="writecode">项目介绍：一款linux性能检测工具，用于检查网络吞吐量，服务器cpu监控，内存监控等信息展示的后台系统。</p>')
+	l.load('<p class="writecode">主要工作：负责页面布局和交互逻辑的实现和后期的性能优化，该系统主要使用了Vue框架的全家桶。项目使用axios与数据库后台交互获取数据，通过axios获取数据并进行数据解析，并且使用了Vuex进行数据存储管理，使用Vue-Router进行路由管理，使用Element组件库搭建的页面，使用了Echarts进行可视化的数据展示，最终将机器的性能监控数据展示到页面。</p>')
+	l.load('<p class="writecode">技术栈：Vue，Vuex，Vue-Router，axios</p>')
 	l.load('<p>&nbsp;</p>')
-	l.load('<h1 class="writecode">自我评价</h1>')
-	l.load('<p>&nbsp;</p>')
-	l.load('<li class="writecode">为人坦诚，守信，肯吃苦, 适应能力强，适应新思维新方式</li>')
-	l.load('<li class="writecode">对于技术有研究精神、攻关能力和创造执行力</li>')
-	l.load('<li class="writecode">学习能力强，能快速的掌握新知识</li>')
-	l.load('<li class="writecode">热爱编程，研发和抗压性强 </li>')
+	
 	
 	
 	let r = new Coder('right')
@@ -468,22 +492,30 @@ $(function(){
 	c.addClass("redc","redc")
 	c.addClass("name","name")
 	c.addClass("val","val")
+	c.addClass("redc","redc")
 	
 	c.load('<p><redc>}</redc></p>')
 	c.load('<p>&nbsp;</p>')
 	
-	r.load('<img src="img/1.jpg" />')
+	c.addClass("choice","choice")
+	c.addClass("redc","redc")
+	c.addClass("name","name")
+	c.addClass("val","val")
+	c.addClass("redc","redc")
+	
+	r.load('<img src="img/1.jpg" style="width:120px;height:160px;"/>')
 	r.load('<h2 class="weight writecode">周善斌</h2>')
 	r.load('<h2 class="weight writecode">求职意向:前端工程师</h2>')
-	r.load('<h2 class="weight writecode">基本信息<p class="weight writecode">性别:男</p><p class="weight writecode">出生日期:1995.08.25</p><p class="weight writecode">籍贯:广西崇左</p><p class="weight writecode">电话:17615195825</p><p class="weight writecode">邮箱:747305307@qq.com</p></h2>')
-	r.load('<h2 class="weight writecode">专业技能<p class="weight writecode">&nbsp;&nbsp;熟悉Vue常用功能和命令，如组件，Vue-Router，Vuex，生命周期等，熟练组件化开发。</p><p class="weight writecode">&nbsp;&nbsp;熟悉HTML5，CSS3，熟悉响应是布局，弹性布局百分比自适应布局，CSS3动画技术。熟悉REM，vw/vh等制作移动端适配页面。</p><p class="weight writecode">&nbsp;&nbsp;熟悉Javascript，ES6，Jquery，掌握Jquery常用API与DOM操作，熟悉面向对象JS编程。</p><p class="weight writecode">&nbsp;&nbsp;熟悉Mint-ui, Element-ui, BootStrap，better-scroll, swiper，ECharts 等ui库和插件的使用。</p><p class="weight writecode">&nbsp;&nbsp;了解Node Express框架的使用，熟练AJAX/JSON，正则表达式，了解git版本管理工具的使用。</p></h2>')
+	r.load('<h2 class="weight writecode">基本信息<p class="weight writecode">性别:男</p><p class="weight writecode">年龄:24</p><p class="weight writecode">籍贯:广西崇左</p><p class="weight writecode">电话:17615195825</p><p class="weight writecode">邮箱:747305307@qq.com</p></h2>')
+	r.load('<p class="writecode text-left">&nbsp;&nbsp;教育经历 ：大连民族大学</p><p class="writecode text-left">&nbsp;&nbsp;学位 ：本科</p><p class="writecode text-left">&nbsp;&nbsp;专业 ：计算机科学与技术</p><p class="writecode text-left">&nbsp;&nbsp;个人GitHub：https://github.com/jaychou1995</p>')
 	
-	c.load('<p>&nbsp;</p>',1500)
-	c.load('<p  class="writecode work">最后非常感谢您愿意花3分钟看完我的简历！</p>')
-	c.load('<p  class="writecode work">我的联系方式</p>')
-	c.load('<p  class="writecode work">电话/微信 17615195825</p>')
-	c.load('<p  class="writecode work">邮箱 747305307@qq.com </p>')
-	c.load('<p  class="writecode work">希望能有一次相互了解的机会,感谢!</p>')
+	
+	c.load('<p>&nbsp;</p>',1200)
+	c.load('<p  class="writecode work">最后非常感谢您愿意花两分钟看完我的简历！</p>',1200)
+	c.load('<p  class="writecode work">我的联系方式</p>',1200)
+	c.load('<p  class="writecode work">电话/微信 17615195825</p>',1200)
+	c.load('<p  class="writecode work">邮箱 747305307@qq.com </p>',1200)
+	c.load('<p  class="writecode work">希望能有一次相互了解的机会,感谢!</p>',1200)
 	
 })
 
